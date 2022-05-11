ceb66c1cf047fd5ff5ca4bb86811dae631c68fb0
James Park <james.hansung.park@gmail.com>
2022-05-10 22:55:47 -0400

    dropdown created

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
+
+// Components
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import Dropdown from './components/Dropdown/Dropdown';
+
+// Layouts
 import ThreeColGrid from './layout/ThreeColGrid';
 import OneTwoColGrid from './layout/OneTwoColGrid';
 import TwoOneColGrid from './layout/TwoOneColGrid';
-import ItemCard from './components/Item Card/ItemCard';
 import OneColGrid from './layout/OneColGrid';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Dropdown />
       <main className="mx-6 md:mx-16">
         <ThreeColGrid />
       </main>
