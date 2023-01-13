56305c334b6e4f174a430ed157923d3a5f95a510
Jamhanpar <james.hansung.park@gmail.com>
2023-01-12 21:19:20 -0500

    add react-icons and logo to nav bar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaEarlybirds } from "react-icons/fa";
 
 export default function Nav() {
   return (
     <div>
       <nav className="flex justify-between p-4 border-b-2 border-gray-200">
-        <div>
+        <div className="flex flex-row items-center gap-2">
+          <FaEarlybirds />
           <h1 className="font-bold">Company</h1>
         </div>
         <ul className="flex gap-4">
