c20c845929f7fa149c5cec063244937fe2af8699
Jamhanpar <james.hansung.park@gmail.com>
2023-01-13 17:21:56 -0500

    add static sidebar

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,19 +1,24 @@
-import { FiMenu } from 'react-icons/fi';
+import { FiMenu, FiHome } from 'react-icons/fi';
+import { FaRegAddressCard } from 'react-icons/fa';
+import { GiShirtButton } from 'react-icons/gi';
 
 export default function Sidebar() {
-  return <div className="w-full h-auto bg-slate-800 text-white">
-    <div>
-        <FiMenu />
-        <h1>Company</h1>
+  return <div className="w-full py-2 bg-slate-800 text-white sm:w-48">
+    <div className="flex flex-row items-center gap-3 mx-3 my-2 cursor-pointer">
+        <FiMenu className="select-none" />
+        <h1>Logo</h1>
     </div>
-    <div>
-        <div>
+    <div className="flex flex-col">
+        <div className="flex flex-row items-center gap-3 mx-3 my-2 cursor-pointer">
+            <FiHome />
             <span>Home</span>
         </div>
-        <div>
+        <div className="flex flex-row items-center gap-3 mx-3 my-2 cursor-pointer">
+            <FaRegAddressCard />
             <span>Cards</span>
         </div>
-        <div>
+        <div className="flex flex-row items-center gap-3 mx-3 my-2 cursor-pointer">
+            <GiShirtButton />
             <span>Buttons</span>
         </div>
     </div>
