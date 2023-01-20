89ba1620fdb6997316e5ae6b735b28620713917c
Jamhanpar <james.hansung.park@gmail.com>
2023-01-19 20:33:07 -0500

    added toggle functionality

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -2,6 +2,7 @@ import TestLayout from '../layout/TestLayout';
 
 // Testing Component
 import Sidebar from '../components/Sidebar/Sidebar';
+import Toggle from '../components/Toggle/Toggle';
 
 export default function Test() {
   return (
@@ -9,6 +10,7 @@ export default function Test() {
       <div className="flex flex-row">
         <Sidebar />
         {/* <div>Content</div> */}
+        <Toggle />
       </div>
     </TestLayout>
   );
