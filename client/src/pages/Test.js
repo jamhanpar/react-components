0bcf88f71590ca5fc55b36032cf3b37435623dcb
Jamhanpar <james.hansung.park@gmail.com>
2023-03-04 17:09:52 -0500

    created flip card

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -3,14 +3,24 @@ import TestLayout from '../layout/TestLayout';
 // Testing Component
 import Sidebar from '../components/Sidebar/Sidebar';
 import Toggle from '../components/Toggle/Toggle';
+import FlipCard from '../components/Cards/FlipCard';
 
 export default function Test() {
   return (
     <TestLayout>
-      <div className="flex flex-row">
-        <Sidebar />
-        {/* <div>Content</div> */}
-        <Toggle />
+      <div className="border border-y-1 border-gray-800">
+        <h1 className="border border-b-gray-800">Sidebar</h1>
+        <div className="flex flex-row">
+          <Sidebar />
+          {/* <div>Content</div> */}
+          <Toggle />
+        </div>
+      </div>
+      <div className="border border-y-1 border-gray-800">
+        <h1 className="border border-b-gray-800">Card Flip</h1>
+        <div className="flex flex-row">
+          <FlipCard />
+        </div>
       </div>
     </TestLayout>
   );
