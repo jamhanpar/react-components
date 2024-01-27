7bf854f0bc7c2c4025efb231de9b7133e253dc8b
Jamhanpar <james.hansung.park@gmail.com>
2024-01-27 00:08:51 -0500

    initial setup

---
+++ components/src/components/Button.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './button.scss';
+import styled from 'styled-components';
+
+interface ButtonProps {
+  classname?: string;
+  buttonType?: 'primary' | 'secondary' | 'tertiary';
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large';
+  label: string;
+  onClick?: () => void;
+}
+
+export const Button = ({
+  classname,
+  buttonType = 'primary',
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  return (
+    <Wrapper
+      type='button'
+      className={`${buttonType} ${classname}`}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
--- components/src/stories/Button.stories.ts
+++ components/src/stories/Button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from './Button';
+import { Button } from '../components/Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -24,7 +24,6 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
     label: 'Button',
   },
 };
