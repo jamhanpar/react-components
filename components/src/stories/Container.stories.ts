477c9a9483caf1e858cd30e81265be1b2bcceb48
Jamhanpar <james.hansung.park@gmail.com>
2024-01-27 22:25:38 -0500

    updated story name

--- components/src/stories/Container.stories.ts
+++ components/src/stories/Container.stories.ts
@@ -14,7 +14,7 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Article: Story = {
   args: {
     classname: 'container',
   },
