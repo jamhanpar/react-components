77cb87119c2fa4aa7e39d3ea3c75cdbda1aca826
James P <james.hansung.park@gmail.com>
2024-08-18 14:26:24 -0400

    react travel spinner using tailwindcss instead of styled components

---
+++ components/src/components/Spinners/EarthAndPlane/EarthAndPlane.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import clsx from 'clsx';
+import Earth from './Earth';
+
+export default function EarthAndPlane() {
+  const [isSpinning, setIsSpinning] = useState(true);
+  return (
+    <div
+      className={clsx(
+        'relative',
+        isSpinning && 'animate-[spin_4s_linear_infinite]'
+      )}
+    >
+      {/* Earth */}
+      <div className={clsx(isSpinning && 'animate-[spin_10s_linear_infinite]')}>
+        <Earth />
+      </div>
+
+      {/* Plane */}
+      <div className='p-2 rounded-full position absolute -bottom-12'>
+        <i className='fa-solid fa-plane rotate-[190deg] text-3xl text-neutral-400'></i>
+      </div>
+    </div>
+  );
+}
---
+++ components/src/components/Spinners/SunAndEarth/SunAndEarth.jsx
@@ -0,0 +1,7 @@
+export default function SunAndEarth() {
+  return (
+    <div className='p-6 rounded-full shadow-xl animate-spin bg-amber-300 border border-amber-100'>
+      <div className='p-2 bg-gradient-to-tr from-blue-400 to-green-300 rounded-full position absolute -bottom-10 transition duration-500 shadow-md'></div>
+    </div>
+  );
+}
